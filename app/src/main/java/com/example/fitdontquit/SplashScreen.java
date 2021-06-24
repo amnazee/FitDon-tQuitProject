b47ef66763eb78f95ca7f97221da45acad21d3e7
Amna Zeeshan <amnazee698@gmail.com>
2021-06-24 19:43:36 +0500

    Commit Two

---
+++ app/src/main/java/com/example/fitdontquit/SplashScreen.java
@@ -0,0 +1,36 @@
+package com.example.fitdontquit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashScreen extends AppCompatActivity {
+    TextView textView;
+    ImageView imageView;
+    Animation up,down;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        imageView=findViewById(R.id.imSplash);
+        textView=findViewById(R.id.txtNameSplash);
+        up= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.up);
+        down= AnimationUtils.loadAnimation(getApplicationContext(),R.anim.down);
+        imageView.setAnimation(up);
+        textView.setAnimation(down);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                finish();
+            }
+        },3500);
+    }
+}
