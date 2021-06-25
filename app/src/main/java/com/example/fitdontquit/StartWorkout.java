ce9ca30bee77b254eebdba4ec104d263511b1d02
Amna Zeeshan <amnazee698@gmail.com>
2021-06-25 18:30:51 +0500

    Edit

--- app/src/main/java/com/example/fitdontquit/StartWorkout.java
+++ app/src/main/java/com/example/fitdontquit/StartWorkout.java
@@ -3,12 +3,58 @@ package com.example.fitdontquit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class StartWorkout extends AppCompatActivity {
+    ImageView imgTimer;
+    TextView timerValue;
+    private static final long START_TIME_IN_MILLIS = 50000;
+    private CountDownTimer countDownTimer;
+    private boolean mTimerRunning;
+    private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
+    Animation alphago;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_workout);
+        imgTimer = (ImageView) findViewById(R.id.imgtimer);
+        timerValue = (TextView) findViewById(R.id.timerValue);
+        timerValue.startAnimation(alphago);
+        imgTimer.startAnimation(alphago);
+        alphago = AnimationUtils.loadAnimation(this, R.anim.alphago);
+
+        startTimer();
+    }
+
+    private void startTimer(){
+        countDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                Toast.makeText(getApplicationContext(),"Done!", Toast.LENGTH_SHORT).show();
+            }
+        }.start();
+        mTimerRunning = true;
+    }
+
+    private void updateCountDownText(){
+        int minutes = (int) (mTimeLeftInMillis / 1000) / 60;
+        int seconds = (int) (mTimeLeftInMillis / 1000) % 60;
+
+        String timeLeft = String.format(Locale.getDefault(),"%02d:%02d", minutes, seconds) ;
+        timerValue.setText(timeLeft);
     }
 }
